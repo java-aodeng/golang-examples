66feca2aeca5b34d915575e294cd8720bd3ea2a3
qq18281714982 <java@aodeng.cc>
2019-06-17 22:58:23 +0800

    :art: go-06

--- go-06/array_test.go
+++ go-06/array_test.go
@@ -2,6 +2,7 @@ package go_06
 
 import "testing"
 
+//数组
 func TestArray(t *testing.T)  {
 	var arr [3]int
 	t.Log(arr[1])
@@ -18,4 +19,18 @@ func TestArrayTravel(t *testing.T)  {
 	}
 
 	t.Log(arr[1:len(arr)])
+}
+
+//切片
+func TestSliceInit(t *testing.T)  {
+	var s[] int
+	t.Log(len(s),cap(s))
+	s=append(s,1)
+	t.Log(len(s),cap(s))
+
+	s1:=[] int{1,2,3}
+	t.Log(len(s1),cap(s1))
+
+	s2:=make ([]int,2,3)
+	t.Log(len(s2),cap(s2))
 }
