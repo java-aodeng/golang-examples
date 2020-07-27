d6d4b23571fe2c329395e7a63d4360fe1d327b4d
java-aodeng <java@aodeng.cc>
2020-07-27 15:11:46 +0800

    init函数

--- go-test-code/code_test.go
+++ go-test-code/code_test.go
@@ -3,6 +3,7 @@ package go_test_code
 import (
 	//自定义包名aodeng
 	aodeng "fmt"
+	"math"
 	"os"
 	"runtime"
 	//test包
@@ -42,4 +43,17 @@ func TestCode3(t *testing.T) {
 	aodeng.Printf("The operating system is: %s\n", goos)
 	path := os.Getenv("PATH")
 	aodeng.Printf("Path is %s\n", path)
+}
+
+//变量除了可以在全局声明中初始化，也可以在 init 函数中初始化。这是一类非常特殊的函数，它不能够被人为调用，而是在每个包完成初始化后自动执行，并且执行优先级比 main 函数高。
+//每个源文件都只能包含一个 init 函数。初始化总是以单线程执行，并且按照包的依赖关系顺序执行。
+//一个可能的用途是在开始执行程序之前对数据进行检验或修复，以保证程序状态的正确性
+var Pi float64
+
+func init() {
+	Pi = 4 * math.Atan(1)
+}
+func TestCode4(t *testing.T) {
+	var twoPi = 2 * Pi
+	aodeng.Print("2*Pi:", twoPi)
 }
