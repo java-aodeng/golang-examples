4a94f64b4771450af4b1ab33754dc4763054b95f
java-aodeng <java@aodeng.cc>
2020-07-27 10:36:32 +0800

    规范代码

--- go-07/map_test.go
+++ go-07/map_test.go
@@ -1,7 +1,6 @@
 package go_07
 
 import (
-	aodeng "fmt"
 	"testing"
 )
 
@@ -16,32 +15,4 @@ func TestTraveMap(t *testing.T) {
 	for k, v := range m {
 		t.Log(k, v)
 	}
-}
-
-func Test1(a *testing.T) {
-	for i := 0; i < 500; i++ {
-		//aa:=1
-		//bb:=2
-	}
-	//根据你的个人喜好对包名进行重新设置
-	aodeng.Print("this is aodeng pkg")
-	aodeng.Print("\n")
-	aodeng.Print(functionName(2))
-	print("aaa")
-	main()
-}
-
-//这是定义一个函数最简单的格式
-func Sum(a, b int) int {
-	return a + b
-}
-
-//因此符合规范的函数一般写成如下的形式
-func functionName(a int) int {
-	return Sum(a, a)
-}
-
-func main() {
-	//如何在 Go 中使用国际化字符
-	aodeng.Printf("Καλημέρα κόσμε; or こんにちは 世界\n")
 }
